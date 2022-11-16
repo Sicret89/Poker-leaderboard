6b3c16daadce5d7efe581ea7cacc1850fafbbafc
Kamil Banas <kamil.banas@tietoevry.com>
2022-11-16 15:13:53 +0100

    Add single event url

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import Index, DashboardView
+from . import views
+from .views import Index, DashboardView, Season
 
 urlpatterns = [
     path("leaderboard_list/", Index.as_view(), name="leaderboard_list"),
-    path('player/<int:pk>/', DashboardView.as_view(), name='dashboard')
+    path('player/<int:pk>/', DashboardView.as_view(), name='dashboard'),
+    path("season/", Season.as_view(), name="season"),
 ]
--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,4 +1,5 @@
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render
 
 from leaderboard.forms import PlayerUpdateForm
 from leaderboard.models import Player
@@ -19,3 +20,22 @@ class DashboardView(SuccessMessageMixin, generic.UpdateView):
     template_name = 'leaderboard/dashboard.html'
     success_url = reverse_lazy('leaderboard_list')
     success_message = "%(name)s was edited successfully."
+
+
+class Season(generic.ListView):
+    model = Player
+    template_name = 'leaderboard/single_event_list.html'
+    paginate_by = 15
+    ordering = ['-total']
+
+    def get_context_data(self, **kwargs):
+        context = super(Season, self).get_context_data(**kwargs)
+        context['season1'] = Player.objects.filter().values('S1E1'),
+        context['season2'] = Player.objects.filter().values('S1E2'),
+        return context
+
+# def season1(request):
+#     context = {
+#         'season1': Player.objects.filter().values('S1E1', 'S1E2')
+#     }
+#     return render(request, 'leaderboard/single_event_list.html', context)
