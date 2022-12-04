2dab055f774f127d342d1f7231f7de0bb6bcbdea
Kamil Banas <kamil.banas@tietoevry.com>
2022-12-04 14:01:12 +0100

    Rename url to sinble_event_list

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -6,5 +6,5 @@ from .views import Index, DashboardView, Season
 urlpatterns = [
     path("leaderboard_list/", Index.as_view(), name="leaderboard_list"),
     path('player/<int:pk>/', DashboardView.as_view(), name='dashboard'),
-    path("season/", Season.as_view(), name="season"),
+    path("single_event_list/", Season.as_view(), name="season"),
 ]
