0d2661eafaa9daae11b63490a17ef3545c13eeea
Kamil Banas <kamil.banas@tietoevry.com>
2022-12-06 14:00:53 +0100

    Refactor code in single_event

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,5 +1,6 @@
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, redirect
 
 from leaderboard.forms import PlayerUpdateForm
 from leaderboard.models import Player
@@ -28,3 +29,14 @@ class Season(generic.ListView):
     # paginate_by = 15
     # ordering = ['-S1E2']
     # queryset = Player.objects.order_by('-total')
+
+    # def get_queryset(self):
+    #     return Player.objects.filter(
+    #         set_name=self.request.GET.get('q')
+    #     )
+    #
+    # def dispatch(self, *args, **kwargs):
+    #     try:
+    #         return super().dispatch(*args, **kwargs)
+    #     except Http404:
+    #         return redirect('core:dashboard')
