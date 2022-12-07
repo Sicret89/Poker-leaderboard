539ff9b5ef27db0956375b3da42bb950566ebf17
Kamil Banas <kamil.banas@tietoevry.com>
2022-12-07 11:01:18 +0100

    Remove Register, and Posts options + urls

--- app/settings.py
+++ app/settings.py
@@ -115,5 +115,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'core-home'
+LOGIN_REDIRECT_URL = 'leaderboard_list'
 LOGIN_URL = 'login'
--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -4,7 +4,7 @@ from . import views
 from .views import Index, DashboardView, Season
 
 urlpatterns = [
-    path("leaderboard_list/", Index.as_view(), name="leaderboard_list"),
+    path("", Index.as_view(), name="leaderboard_list"),
     path('player/<int:pk>/', DashboardView.as_view(), name='dashboard'),
     path("single_event_list1/", Season.as_view(), name="season1"),
     path("single_event_list2/", Season.as_view(), name="season2"),
