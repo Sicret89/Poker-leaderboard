32c365cb3aece6e8b81f7cb189fa9bb548583b60
Kamil Banas <kamil.banas@tietoevry.com>
2022-12-14 17:16:48 +0100

    CRF Test

--- app/settings.py
+++ app/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = str(os.environ.get('DEBUG')) == '0' # 1 == True
 
-# CSRF_TRUSTED_ORIGINS = ['https://akvs47.herokuapp.com', 'www.akvs47.club']
+CSRF_TRUSTED_ORIGINS = ['https://akvs47.herokuapp.com']
 
 ALLOWED_HOSTS = ['akvs47.herokuapp.com', 'akvs47.club']
 if not DEBUG:
