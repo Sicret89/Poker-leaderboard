889c72affb8ad2b09cb7970a972d93315067bd94
Kamil Banas <kamil.banas@tietoevry.com>
2022-12-15 12:17:32 +0100

    Add jocker field to model csv importer and leaderbord

--- csvs/views.py
+++ csvs/views.py
@@ -18,14 +18,19 @@ def upload_file_view(request):
                 season = row1[0]
                 bonus_ak = season + 'B_AK'
                 bonus_47 = season + 'B_47'
+                jocker = season + '_JOCKER'
                 single, _ = Player.objects.update_or_create(
                     name=row1[1],
                     defaults={
                         **{season: row1[2] if row1[2] else 0, },
                         **{bonus_ak: row1[3] if row1[3] else 0, },
                         **{bonus_47: row1[4] if row1[4] else 0, },
+                        **{jocker: row1[6] if row1[6] else None, },
                     },
                 )
+                print('-----------------------------------------------------')
+                print(row1)
+                print('-----------------------------------------------------')
             obj.activated = True
             obj.save()
     return render(request, 'leaderboard/upload.html', {'form': form})
--- leaderboard/models.py
+++ leaderboard/models.py
@@ -23,39 +23,51 @@ class Player(models.Model):
     E1 = models.IntegerField(default=0)
     E1B_AK = models.IntegerField(default=0)
     E1B_47 = models.IntegerField(default=0)
+    E1_JOCKER = models.BooleanField(null=True, default=None)
     E2 = models.IntegerField(default=0)
     E2B_AK = models.IntegerField(default=0)
     E2B_47 = models.IntegerField(default=0)
+    E2_JOCKER = models.BooleanField(null=True, default=None)
     E3 = models.IntegerField(default=0)
     E3B_AK = models.IntegerField(default=0)
     E3B_47 = models.IntegerField(default=0)
+    E3_JOCKER = models.BooleanField(null=True, default=None)
     E4 = models.IntegerField(default=0)
     E4B_AK = models.IntegerField(default=0)
     E4B_47 = models.IntegerField(default=0)
+    E4_JOCKER = models.BooleanField(null=True, default=None)
     E5 = models.IntegerField(default=0)
     E5B_AK = models.IntegerField(default=0)
     E5B_47 = models.IntegerField(default=0)
+    E5_JOCKER = models.BooleanField(null=True, default=None)
     E6 = models.IntegerField(default=0)
     E6B_AK = models.IntegerField(default=0)
     E6B_47 = models.IntegerField(default=0)
+    E6_JOCKER = models.BooleanField(null=True, default=None)
     E7 = models.IntegerField(default=0)
     E7B_AK = models.IntegerField(default=0)
     E7B_47 = models.IntegerField(default=0)
+    E7_JOCKER = models.BooleanField(null=True, default=None)
     E8 = models.IntegerField(default=0)
     E8B_AK = models.IntegerField(default=0)
     E8B_47 = models.IntegerField(default=0)
+    E8_JOCKER = models.BooleanField(null=True, default=None)
     E9 = models.IntegerField(default=0)
     E9B_AK = models.IntegerField(default=0)
     E9B_47 = models.IntegerField(default=0)
+    E9_JOCKER = models.BooleanField(null=True, default=None)
     E10 = models.IntegerField(default=0)
     E10B_AK = models.IntegerField(default=0)
     E10B_47 = models.IntegerField(default=0)
+    E10_JOCKER = models.BooleanField(null=True, default=None)
     E11 = models.IntegerField(default=0)
     E11B_AK = models.IntegerField(default=0)
     E11B_47 = models.IntegerField(default=0)
+    E11_JOCKER = models.BooleanField(null=True, default=None)
     E12 = models.IntegerField(default=0)
     E12B_AK = models.IntegerField(default=0)
     E12B_47 = models.IntegerField(default=0)
+    E12_JOCKER = models.BooleanField(null=True, default=None)
     _total = None
 
     objects = PlayerManager(
@@ -64,18 +76,44 @@ class Player(models.Model):
               F('E6') + F('E6B_AK') + F('E6B_47') + F('E7') + F('E7B_AK') + F('E7B_47') + F('E8') + F('E8B_AK') +
               F('E8B_47') + F('E9') + F('E9B_AK') + F('E9B_47') + F('E10') + F('E10B_AK') + F('E10B_47') +
               F('E11') + F('E11B_AK') + F('E11B_47') + F('E12') + F('E12B_AK') + F('E12B_47'),
+        total_jocker_e1=(F('E1') + F('E1B_AK') + F('E1B_47')) * 2 + F('E2') + F('E2B_AK') + F('E2B_47') + F('E3') +
+                        F('E3B_AK') + F('E3B_47') + F('E4') + F('E4B_AK') + F('E4B_47') + F('E5') + F('E5B_AK') + F(
+            'E5B_47') +
+                        F('E6') + F('E6B_AK') + F('E6B_47') + F('E7') + F('E7B_AK') + F('E7B_47') + F('E8') + F(
+            'E8B_AK') +
+                        F('E8B_47') + F('E9') + F('E9B_AK') + F('E9B_47') + F('E10') + F('E10B_AK') + F('E10B_47') +
+                        F('E11') + F('E11B_AK') + F('E11B_47') + F('E12') + F('E12B_AK') + F('E12B_47'),
+        total_jocker_e2=F('E1') + F('E1B_AK') + F('E1B_47') + (F('E2') + F('E2B_AK') + F('E2B_47')) * 2 + F('E3') +
+                        F('E3B_AK') + F('E3B_47') + F('E4') + F('E4B_AK') + F('E4B_47') + F('E5') + F('E5B_AK') + F(
+            'E5B_47') +
+                        F('E6') + F('E6B_AK') + F('E6B_47') + F('E7') + F('E7B_AK') + F('E7B_47') + F('E8') + F(
+            'E8B_AK') +
+                        F('E8B_47') + F('E9') + F('E9B_AK') + F('E9B_47') + F('E10') + F('E10B_AK') + F('E10B_47') +
+                        F('E11') + F('E11B_AK') + F('E11B_47') + F('E12') + F('E12B_AK') + F('E12B_47'),
         total_e1=F('E1') + F('E1B_AK') + F('E1B_47'),
+        total_e1_jocker=(F('E1') + F('E1B_AK') + F('E1B_47')) * 2,
         total_e2=F('E2') + F('E2B_AK') + F('E2B_47'),
+        total_e2_jocker=(F('E2') + F('E2B_AK') + F('E2B_47')) * 2,
         total_e3=F('E3') + F('E3B_AK') + F('E3B_47'),
+        total_e3_jocker=(F('E3') + F('E3B_AK') + F('E3B_47')) * 2,
         total_e4=F('E4') + F('E4B_AK') + F('E4B_47'),
+        total_e4_jocker=(F('E4') + F('E4B_AK') + F('E4B_47')) * 2,
         total_e5=F('E5') + F('E5B_AK') + F('E5B_47'),
+        total_e5_jocker=(F('E5') + F('E5B_AK') + F('E5B_47')) * 2,
         total_e6=F('E6') + F('E6B_AK') + F('E6B_47'),
+        total_e6_jocker=(F('E6') + F('E6B_AK') + F('E6B_47')) * 2,
         total_e7=F('E7') + F('E7B_AK') + F('E7B_47'),
+        total_e7_jocker=(F('E7') + F('E7B_AK') + F('E7B_47')) * 2,
         total_e8=F('E8') + F('E8B_AK') + F('E8B_47'),
+        total_e8_jocker=(F('E8') + F('E8B_AK') + F('E8B_47')) * 2,
         total_e9=F('E9') + F('E9B_AK') + F('E9B_47'),
+        total_e9_jocker=(F('E9') + F('E9B_AK') + F('E9B_47')) * 2,
         total_e10=F('E10') + F('E10B_AK') + F('E10B_47'),
+        total_e10_jocker=(F('E10') + F('E10B_AK') + F('E10B_47')) * 2,
         total_e11=F('E11') + F('E11B_AK') + F('E11B_47'),
+        total_e11_jocker=(F('E11') + F('E11B_AK') + F('E11B_47')) * 2,
         total_e12=F('E12') + F('E12B_AK') + F('E12B_47'),
+        total_e12_jocker=(F('E12') + F('E12B_AK') + F('E12B_47')) * 2,
     )
 
     def __str__(self):
