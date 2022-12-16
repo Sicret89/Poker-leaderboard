b367cde577dd994265d4ade717417bcdb50c54bb
Kamil Banas <kamil.banas@tietoevry.com>
2022-12-16 15:41:38 +0100

    Reverse back staticfiles dirs

--- app/settings.py
+++ app/settings.py
@@ -109,9 +109,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
