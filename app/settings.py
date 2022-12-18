47e5525ff9913689a72752aca81dd5c767d5503c
Kamil Banas <kamil.banas@tietoevry.com>
2022-12-18 08:52:34 +0100

    Disable DEFAULT_FILE_STORAGE s3 for now

--- app/settings.py
+++ app/settings.py
@@ -137,7 +137,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_S3_REGION_NAME = "eu-west-2"
 #
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #
 
 
